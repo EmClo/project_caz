c009fde94ce4759b7c414b4d9534fc317d3fff99
emmavnorthcott <emnorthcott@gmail.com>
2022-07-31 20:46:18 +0200

    Added private vehicle question

---
+++ reg_number.py
@@ -0,0 +1,5 @@
+def reg_number_input():
+    reg_number = (input("What is your registration number? ")).strip()
+    private_vehicle = (input("Is your car a private vehicle? Yes/No "))
+    return reg_number, private_vehicle
+
--- reg_number_input.py
+++
@@ -1,6 +0,0 @@
-def reg_number():
-    reg_number = (input("What is your registration number? ")).strip()
-    print(reg_number)
-
-reg_number()
-
