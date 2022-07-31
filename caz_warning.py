cf685bfcba2666b4a96e3d6033b6fea50983d910
emmavnorthcott <emnorthcott@gmail.com>
2022-07-31 17:12:35 +0200

    Added warning to function.

--- caz_warning.py
+++ caz_warning.py
@@ -3,7 +3,7 @@ enter_caz = True
 caz_liable = True
 link = "https://pay.drive-clean-air-zone.service.gov.uk/vehicles/enter_details"
 
-def caz_zone_warning():
+def caz_zone_warning(enter_caz):
     if enter_caz == True and caz_liable == True:
         print(f"You're about to enter the Clean Air zone. Please pay here {link} or re-route your journey. ")
     elif enter_caz == True and caz_liable == False:
