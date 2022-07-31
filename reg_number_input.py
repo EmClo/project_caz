a991ff0df0e7db135f093338d019b5a9e1beeb16
emmavnorthcott <emnorthcott@gmail.com>
2022-07-31 11:23:39 +0200

    Create reg number input variable with strip

--- reg_number_input.py
+++ reg_number_input.py
@@ -1,4 +1,3 @@
-reg_number = (input("What is your car's registration number? "))
+reg_number = (input("What is your registration number? ")).strip()
 print(reg_number)
 
-# We could also add something to very if the reg number, but getting the rules for vanity paltes was hard. I think we just match against the DVLA list and return an error if it's not there.
