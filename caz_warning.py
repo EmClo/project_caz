69568259dba0eaccfeaf3aef515c2ce52383510b
emmavnorthcott <emnorthcott@gmail.com>
2022-07-31 20:53:25 +0200

    update variable names

--- caz_warning.py
+++ caz_warning.py
@@ -1,14 +1,16 @@
+from test_db import get_caz_liable
+from reg_number import reg_number_input
 
+caz_compliant = get_caz_liable(reg_number_input())
 enter_caz = True
-caz_liable = True
 link = "https://pay.drive-clean-air-zone.service.gov.uk/vehicles/enter_details"
 
-def caz_zone_warning(enter_caz):
-    if enter_caz == True and caz_liable == True:
+def caz_zone_warning(enter_caz, caz_compliant):
+    if enter_caz == True and caz_compliant == False:
         print(f"You're about to enter the Clean Air zone. Please pay here {link} or re-route your journey. ")
-    elif enter_caz == True and caz_liable == False:
+    elif enter_caz == True and caz_compliant == True:
         print("You're free to drive through the Clean Air Zone. ")
     else:
         print("Your journey does not take you through the clean air zone. ")
 
-caz_zone_warning()
+caz_zone_warning(enter_caz, caz_compliant)
