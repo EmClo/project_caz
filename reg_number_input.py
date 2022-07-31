252a92520a588224fed9772899c43a36c7d35074
emmavnorthcott <emnorthcott@gmail.com>
2022-07-31 12:57:04 +0200

    Create warning message for caz.

--- reg_number_input.py
+++ reg_number_input.py
@@ -1,4 +1,6 @@
-reg_number = (input("What is your car's registration number? "))
-print(reg_number)
+def reg_number():
+    reg_number = (input("What is your registration number? ")).strip()
+    print(reg_number)
 
-# We could also add something to very if the reg number, but getting the rules for vanity paltes was hard. I think we just match against the DVLA list and return an error if it's not there.
+reg_number()
+
