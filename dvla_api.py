34589102b8374196582490d338602945e7cc1803
olivia5484 <olivia5484m@gmail.com>
2022-08-12 14:09:09 +0100

    updated vehicle exemption (with more options for non-private vehicles) commit

--- dvla_api.py
+++ dvla_api.py
@@ -23,6 +23,8 @@ def vehicle_data():
     euro6_num = datetime.strptime(euro6_van_date, '%Y-%m')
     returned_date = registration_data
     reg_date_num = datetime.strptime(returned_date, '%Y-%m')
+    euro6_car_date = "2015-01"
+    euro6_car_num = datetime.strptime(euro6_car_date, '%Y-%m')
 
     is_private = input("Is your vehicle a private vehicle? Please enter Y or N: ")
     if is_private == 'Y' and vehicle_type == 'M1':
@@ -32,7 +34,13 @@ def vehicle_data():
         print("Please visit: https://beta.bathnes.gov.uk/check-your-vehicle-and-pay-charge.")
     elif is_private == 'Y' and vehicle_type == 'N1' and reg_date_num > euro6_num:
         print("Your van is exempt from CAZ charges. ")
-    elif is_private == 'N':
+    elif is_private == 'N' and vehicle_type == 'M1' and reg_date_num > euro6_car_num:
+        print("Your vehicle is likely to be exempt.")
+        print("You can check for certain here: https://www.gov.uk/clean-air-zones")
+    elif is_private == 'N' and vehicle_type == 'N1' and reg_date_num > euro6_num:
+        print("Your vehicle is likely to be exempt.")
+        print("You can check for certain here: https://www.gov.uk/clean-air-zones")
+    elif is_private == 'N' and reg_date_num < euro6_num:
         print("Your vehicle is unlikely to be exempt.")
         print("Please visit: https://beta.bathnes.gov.uk/check-your-vehicle-and-pay-charge.")
     else:
