109d48cbddf6d9ad2805598cd12aa277f05b3bfc
olivia5484 <olivia5484m@gmail.com>
2022-08-14 18:31:28 +0100

    debug info commit

--- debug.py
+++ debug.py
@@ -1,20 +1,18 @@
 import string
-
+import sys
 
 def correct_license(user_reg):
     if user_reg.isalnum() and user_reg in user_reg.upper():
         return True
     else:
-        print("That was not a valid license plate. Hint: don't include a space. ")
-        return ValueError
+        raise sys.exit("That is not a correct license plate number. ")
 
 
 def correct_private(is_private):
     if is_private == 'Y' or 'N':
         return True
     else:
-        print("That was not a valid input. Please enter Y or N. ")
-        return ValueError
+        raise sys.exit("That was not a valid option. ")
 
 
 correct_license('MJ59LVZ')              # should return True
